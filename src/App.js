5508862756f667e5df37b1774b4f24ff3c3d9c1c
Abhinav-Rawat <rawatabhi14@gmail.com>
2020-01-13 00:44:43 +0530

    Initial plus Final Commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
 import React, { Component } from 'react';
-import Try from './Try.js';
-import On from './Try.js';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { data: ['HELLO', 'Second', 'Third'], typed: "" };
+  }
   render() {
     return (
       <div className="App">
-        {/* <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <strong>
-          <h2>Welcome to React</h2>
-          </strong>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p> */}
-        <Try title = "React" body = "A simple App" imageUrl = "" isOpen ={false}/>
-        {/* <On /> */}
+        <h1> Todo App </h1>
+        <input type="text" value={this.state.typed} onChange={(evt) => {
+          console.log(evt.target.value);
+          this.setState({ typed: evt.target.value });
+        }}></input>
+        <button onClick={() => {
+          this.setState({ data: this.state.data.concat(this.state.typed), typed: "" });
+        }}>Add</button>
+        <ul>
+          {this.state.data.map((d, i) => <li key={i}> {d} <a href="#" onClick={() => {
+            let new_data = [...this.state.data];
+            new_data.splice(i, 1);
+            this.setState({data: new_data});
+          }}>x</a> </li>)}
+        </ul>
       </div>
     );
   }
 }
+
 
 export default App;
